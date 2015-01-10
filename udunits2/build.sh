b89469d6cefcfb6f924323657072aed9f9a81815
ocefpaf <ocefpaf@gmail.com>
2015-01-10 11:07:31 -0300

    Added udunits2.

---
+++ udunits2/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -f configure ];
+then
+   # Make the configure file. Need autoreconf, libtool, libexpat-dev for this.
+   autoreconf -i --force
+fi
+
+./configure --prefix=$PREFIX
+make
+make install
