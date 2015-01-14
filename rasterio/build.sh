57c0d05a81728c49c980b6660bf9582944f52d2e
comtbot <comtbot@gmail.com>
2015-01-14 15:34:09 -0600

    rasterio recipe from shoyer

--- rasterio/build.sh
+++ rasterio/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+UNAME=$(uname -s)
+if [ ${UNAME} = 'Darwin' ]; then
+    export CFLAGS="-mmacosx-version-min=`xcrun --sdk macosx --show-sdk-version`"
+    export CXXFLAGS="$CFLAGS"
+    export LDFLAGS="$CFLAGS"
+fi
+
 $PYTHON setup.py install
 
 # Add more build steps here, if they are necessary.
