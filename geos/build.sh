67e02de2a59f3c3915bfe7692190ad209f04f8dd
ocefpaf <ocefpaf@gmail.com>
2015-03-11 16:38:24 -0300

    Build on RHEL6  (conda-recipes-scitools #54).

--- geos/build.sh
+++ geos/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-./configure --prefix=$PREFIX --without-jni
+
+# Problems with cartopy if the -64 flag is not defined. See https://taskman.eionet.europa.eu/issues/14817.
+CFLAGS="-m64" CPPFLAGS="-m64" CXXFLAGS="-m64" LDFLAGS="-m64" FFLAGS="-m64" ./configure --prefix=$PREFIX --without-jni
 make
 make install
