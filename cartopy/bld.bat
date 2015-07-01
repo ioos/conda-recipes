2669499543483be6a866a63fe290abc7ce58bf87
ocefpaf <ocefpaf@gmail.com>
2015-07-01 12:14:41 -0300

    Update cartopy to v0.13.0

--- cartopy/bld.bat
+++ cartopy/bld.bat
@@ -2,4 +2,4 @@ set LIB=%LIBRARY_LIB%;%LIB%
 set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 
-%PYTHON% setup.py install --single-version-externally-managed  --record record.txt
+%PYTHON% setup.py install --single-version-externally-managed --record record.txt
