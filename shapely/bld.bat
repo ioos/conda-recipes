eb36aa85bd7014981d36c49df2ea45ae31ba9076
ocefpaf <ocefpaf@gmail.com>
2015-07-31 13:44:24 -0300

    Update Shapely to 1.5.9

--- shapely/bld.bat
+++ shapely/bld.bat
@@ -1,6 +1,6 @@
 set LIB=%LIBRARY_LIB%;%LIB%
 set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
-set INCLUDE=%LIBRARY_INC%;%INCLUDE%
+set INCLUDE=%LIBRARY_INC%;%INCLUDE%;%RECIPE_DIR%
 
 %PYTHON% setup.py build_ext -I %LIBRARY_INC% -L %LIBRARY_LIB% -l geos_c
 %PYTHON% setup.py install
