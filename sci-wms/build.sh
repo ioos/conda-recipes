5680c8a6abcbcc6f5056fa2a00ea5828cfacbd7f
ocefpaf <ocefpaf@gmail.com>
2015-08-12 14:24:47 -0300

    Better metapackages

--- sci-wms/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 1.1.0 > __conda_version__.txt
-echo 0 > __conda_buildstr__.txt
-# echo 10 > __conda_buildnum__.txt
-
-# echo "/opt/anaconda1anaconda2anaconda3" > $PREFIX/test-has-prefix
