dcbb5b8ff5b9ffa5176c67722dacd9b415c664db
ocefpaf <ocefpaf@gmail.com>
2015-08-31 09:20:35 -0300

    libmo_unpack patch

--- libmo_unpack/build.sh
+++ libmo_unpack/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 mkdir m4
+
+# Patch the configure.ac as per https://lists.ubuntu.com/archives/fwts-devel/2013-June/003391.html
+sed -i".ac" -e 's/AM_PROG_AR/m4_ifdef([AM_PROG_AR], [AM_PROG_AR])/g' configure.ac
 autoreconf --install
 
 CFLAGS="-O3 -mfpmath=sse -msse"
