bfca524406738d46b15fde5463e2e09154eab1d8
ocefpaf <ocefpaf@gmail.com>
2015-09-24 07:39:08 -0300

    Update rdflib to 4.2.1

--- rdflib/bld.bat
+++ rdflib/bld.bat
@@ -1,3 +1,6 @@
+:: Ugly hack to avoid circular dependency issue.
+:: (See https://github.com/RDFLib/rdflib/issues/391).
+
 pip install SPARQLWrapper
 
 "%PYTHON%" setup.py install  --single-version-externally-managed  --record record.txt
--- rdflib/build.sh
+++ rdflib/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Ugly hack to avoid circular dependency issue.
+# (See https://github.com/RDFLib/rdflib/issues/391).
+
 pip install SPARQLWrapper
 
 $PYTHON setup.py install --single-version-externally-managed  --record record.txt
