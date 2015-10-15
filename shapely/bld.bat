57342ed77da3431949019e7e85d716a5927ec6ce
ocefpaf <ocefpaf@gmail.com>
2015-10-15 16:51:35 -0300

    Revert to Shapely 1.5.12

--- shapely/bld.bat
+++ shapely/bld.bat
@@ -2,7 +2,7 @@ set LIB=%LIBRARY_LIB%;%LIB%
 set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%;%RECIPE_DIR%
 
-set GEOS_LIBRARY_PATH=%LIBRARY_BIN%\geos_c.dll
+set GEOS_LIBRARY_PATH=%LIBRARY_BIN%\\geos_c.dll
 
 %PYTHON% setup.py build_ext -I %LIBRARY_INC% -L %LIBRARY_LIB% -l geos_c
 %PYTHON% setup.py install
