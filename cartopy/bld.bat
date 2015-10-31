13635ee917021d076ecb50ba1dd24fc61b94cde5
ocefpaf <ocefpaf@gmail.com>
2015-10-31 09:14:58 -0300

    Smaller sdist

--- cartopy/bld.bat
+++ cartopy/bld.bat
@@ -2,5 +2,6 @@ set LIB=%LIBRARY_LIB%;%LIB%
 set LIBPATH=%LIBRARY_LIB%;%LIBPATH%
 set INCLUDE=%LIBRARY_INC%;%INCLUDE%
 
+rmdir lib\cartopy\tests\mpl\baseline_images /s /q
 %PYTHON% setup.py install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
