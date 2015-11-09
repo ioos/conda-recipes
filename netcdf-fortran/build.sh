4702b2dda612131ad61703c4fc3f48e1884ce2b5
ocefpaf <ocefpaf@gmail.com>
2015-11-09 09:46:06 -0300

    kludge

--- netcdf-fortran/build.sh
+++ netcdf-fortran/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ "$(uname)" == "Darwin" ]; then
+    export DYLD_LIBRARY_PATH=${PREFIX}/lib:${PREFIX}/lib64
+else
+    export LD_LIBRARY_PATH=${PREFIX}/lib:${PREFIX}/lib64
+fi
+
 CPPFLAGS=-I$PREFIX/include LDFLAGS=-L$PREFIX/lib ./configure --prefix=$PREFIX
 
 make
