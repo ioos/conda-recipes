65a1d237d74757d2f08ab868b26344d995611f6f
bekozi <benkoziol@gmail.com>
2016-01-29 10:43:49 -0700

    Switched to serial build for ESMF OSX
    Once default channel OSX MPI libraries are in working order we can
    switch back to parallel builds.

--- esmf/build.sh
+++ esmf/build.sh
@@ -7,7 +7,7 @@ export ESMF_NETCDF="split"
 export ESMF_NETCDF_INCLUDE=${PREFIX}/include
 export ESMF_NETCDF_LIBPATH=${PREFIX}/lib
 if [ "$(uname)" == "Darwin" ]; then
-    export ESMF_COMM=openmpi
+    export ESMF_COMM=mpiuni
 else
     export ESMF_COMM=mpich2
 fi
