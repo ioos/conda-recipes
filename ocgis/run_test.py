c3650748a5101921f24a14ccc4ebcea874b84488
bekozi <benkoziol@gmail.com>
2016-03-02 14:44:41 -0700

    WIP: Removed krb5 dependency

--- ocgis/run_test.py
+++ ocgis/run_test.py
@@ -1,11 +1,4 @@
-from ocgis.test import run_simple, run_no_esmf
-import os
+from ocgis.test import run_simple
 
 
-test_target = os.environ.get('CBUILD_OCGIS_TEST_TARGET')
-if test_target == 'simple' or test_target is None:
-    run_simple()
-elif test_target == 'all':
-    run_no_esmf()
-else:
-    raise NotImplementedError(test_target)
+run_simple()
